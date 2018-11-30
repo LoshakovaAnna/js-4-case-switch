1b19c605245d021ee0461a6776643046b3520906
LoshakovaAnna <ann.loshakova.111@gmail.com>
2018-11-30 16:54:31 +0300

    use switch, clean code

--- main.js
+++ main.js
@@ -3,43 +3,47 @@ document.addEventListener("DOMContentLoaded", init);
 function init(){
   
     let btn = document.querySelector(".calculate");
-    btn.addEventListener('click', function(e) {
-        let w = document.querySelector(".weight").value;
-        let h = document.querySelector(".height").value;
-       
-        let imt = parseFloat(w) /(parseFloat(h) *parseFloat(h));
-       
-        var   result;  
-        if (imt <= 16){
-            result = "дефецит ("+imt+")";
-        }
-        else
-        if    (imt<=18.5) {
-            result = "недостаточный ("+imt+")";    
-        }
-        else
-            if(imt<=25){
-                result = "нормальный("+imt+")";
-            }
-        else
-        if (imt<=30){
-            result = "избыточный("+imt+")";
-        }
-        else 
-        if (imt<=35){
-            result = "ожирение 1й("+imt+")";
-        }else
-        if  (imt<=40){
-            result = "ожирение 2й("+imt+")";
-        }
-        else {
-            result = "ожирение 3й("+imt+")";
-        }
-        let answer = document.querySelector(".result");
-        answer.value = result;
-        answer.textContent=result;
-
-    });
-    
-
+    btn.addEventListener("click", calculate);
 }
+
+function calculate(){    
+    let weight = document.querySelector(".weight").value;
+    let height = document.querySelector(".height").value;
+   
+    let indexBodyMass = parseFloat(weight) / (parseFloat(height) * parseFloat(height));
+   
+    let   result;  
+        
+    switch (true) {
+        case (indexBodyMass <= 16):
+        result = "дефецит";
+            break;
+        case ((indexBodyMass > 16) && (indexBodyMass <= 18.5)):
+            result = "недостаточный ";
+            break;
+        case ((indexBodyMass > 18.5) && (indexBodyMass <= 25)):
+            result = "нормальный ";
+            break;
+        case ((indexBodyMass > 25) && (indexBodyMass <= 30)):
+            result = "избыточный ";
+            break;
+        case ((indexBodyMass > 30) && (indexBodyMass <= 35)):
+            result = "ожирение 1й ";
+            break;
+        case ((indexBodyMass > 35) && (indexBodyMass <= 40)):
+            result = "ожирение 2й ";
+            break;
+        case (indexBodyMass > 40):
+            result = "ожирение 3й ";
+            break;
+        
+        default:
+            result = "you do something wrong ";
+            break;
+    } 
+
+    let answer = document.querySelector(".result");
+    answer.value = result;
+    answer.textContent =  result + "(ИМТ = " + indexBodyMass + ")";
+
+};
